2fb319f4198f56408dff7ae4fe3c91d5cecf5e4d
Arpan Nema <arpan.nema.official@gmail.com>
2024-09-30 13:21:43 +1000

    Landing page desktop and mobile view

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -48,60 +48,100 @@ const LandingPage= ()=>{
 	return (
 		<>
 			<MessageDisplay msg={msg} setMsg={setMsg} />
-			<Form ref={usrForm} onSubmit={handleSubmit} className="d-flex h-100">
-				<div className="row flex-grow-1">
-					<div className="col-md-5 LandingPageMain">
-						<div className="d-flex h-100 justify-content-center align-items-center">
-							<div style={{ textAlign: 'left', marginTop: '-15em', width:"50%" }}>
-								<InputField name="PhoneNumber" type="tel" label="Phone Number" />
-								<FormSubmitButton name="Send OTP" />
-							</div>
-						</div>
-					</div>
-					<div className="col-md-6">
-						<div className="h-100 justify-content-center align-items-center">
-							<div className="row m-4">
-								<div className="col-md-12 col-sm-12">
-									<img src={"./ParagraphText.png"} alt="Landing Page Image showing business" width={"75%"} height={"auto"} />
-								</div>
-							</div>
-							<div className="row m-4">
-								<div className="col-md-12 col-sm-12">
-									<center style={{ fontSize:"30px" }}>
-										Streamlined tracking and reconciliation for your purchase agreements
-									</center>
-								</div>
-							</div>
-							<div className="row m-5">
-								<div className="col-md-4">
-									<div className="col-md-12">
-										<img src={"./TrackManageProgress1.png"} alt="Landing Page Image showing business" width={"50%"} height={"auto"} />
-									</div>
-									<div className="col-md-12 landingMsgSubTitle">
-										Track & manage progress
-									</div>
-								</div>
-								<div className="col-md-4">
-									<div className="col-md-12">
-										<img src={"./TrackManageProgress2.png"} alt="Landing Page Image showing business" width={"50%"} height={"auto"} />
-									</div>
-									<div className="col-md-12 landingMsgSubTitle">
-										Reconcile work & payments
-									</div>
-								</div>
-								<div className="col-md-4">
-									<div className="col-md-12">
-										<img src={"./TrackManageProgress3.png"} alt="Landing Page Image showing business" width={"50%"} height={"auto"} />
-									</div>
-									<div className="col-md-12 landingMsgSubTitle">
-										Easy communication & audit
-									</div>
+			<Form ref={usrForm} onSubmit={handleSubmit} >
+				<div className="">
+					<div className="row overflow-auto">
+						<div className="col-md-5 LandingPageMain col-sm-12">
+							<div className="">
+								<div style={{ textAlign: 'left'}}>
+									<InputField name="PhoneNumber" type="tel" label="Phone Number" />
+									<FormSubmitButton name="Send OTP" />
 								</div>
 							</div>
 						</div>
-						
+						<div className="col-md-6 col-sm-12">
+							<div className="d-sm-none d-md-block">
+								<div className="row m-4">
+									<div className="col-md-12 col-sm-12">
+										<img src={"./ParagraphText.png"} alt="Landing Page Image showing business" width={"80%"} />
+									</div>
+								</div>
+								<div className="row m-4">
+									<div className="col-md-12">
+										<center style={{ fontSize: "30px" }}>
+											Streamlined tracking and reconciliation for your purchase agreements
+										</center>
+									</div>
+								</div>
+								<div className="row m-5">
+									<div className="col-md-4">
+										<div className="col-md-12">
+											<img src={"./TrackManageProgress1.png"} alt="Landing Page Image showing business" width={"50%"} height={"auto"} />
+										</div>
+										<div className="col-md-12 landingMsgSubTitle">
+											Track & manage progress
+										</div>
+									</div>
+									<div className="col-md-4">
+										<div className="col-md-12">
+											<img src={"./TrackManageProgress2.png"} alt="Landing Page Image showing business" width={"50%"} height={"auto"} />
+										</div>
+										<div className="col-md-12 landingMsgSubTitle">
+											Reconcile work & payments
+										</div>
+									</div>
+									<div className="col-md-4">
+										<div className="col-md-12">
+											<img src={"./TrackManageProgress3.png"} alt="Landing Page Image showing business" width={"50%"} height={"auto"} />
+										</div>
+										<div className="col-md-12 landingMsgSubTitle">
+											Easy communication & audit
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="d-md-none d-sm-block" style={{ overflowY: "scroll", height: "70%" }}>
+								<div className="row m-4">
+									<div className="col-md-12 col-sm-12">
+										<img src={"./ParagraphText.png"} alt="Landing Page Image showing business" width={"80%"} />
+									</div>
+								</div>
+								<div className="row m-4">
+									<div className="col-md-12">
+										<center style={{ fontSize: "30px" }}>
+											Streamlined tracking and reconciliation for your purchase agreements
+										</center>
+									</div>
+								</div>
+								<div className="row m-5">
+									<div className="col-md-4">
+										<div className="col-md-12">
+											<img src={"./TrackManageProgress1.png"} alt="Landing Page Image showing business" width={"50%"} height={"auto"} />
+										</div>
+										<div className="col-md-12 landingMsgSubTitle">
+											Track & manage progress
+										</div>
+									</div>
+									<div className="col-md-4">
+										<div className="col-md-12">
+											<img src={"./TrackManageProgress2.png"} alt="Landing Page Image showing business" width={"50%"} height={"auto"} />
+										</div>
+										<div className="col-md-12 landingMsgSubTitle">
+											Reconcile work & payments
+										</div>
+									</div>
+									<div className="col-md-4">
+										<div className="col-md-12">
+											<img src={"./TrackManageProgress3.png"} alt="Landing Page Image showing business" width={"50%"} height={"auto"} />
+										</div>
+										<div className="col-md-12 landingMsgSubTitle">
+											Easy communication & audit
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
-					
 				</div>
 			</Form>
 		</>
